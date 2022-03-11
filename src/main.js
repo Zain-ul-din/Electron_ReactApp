7d884463db3a9c686de1ccc2bbdb741f9144a9e3
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-03-12 00:36:25 +0500

    Update main.js
    Electron Fix

--- src/main.js
+++ src/main.js
@@ -49,5 +49,7 @@ app.on('activate', () => {
   }
 });
 
+// npm i electron@latest
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
